6c4c340fdf30464fa66369313b921f68d2110d40
NGOC DUC <vungocduc@VN-HCM-D-W-0007.globalservs.com>
2020-03-26 11:31:44 +0700

    init

---
+++ src/demo/DemoGuru99.java
@@ -0,0 +1,45 @@
+package demo;
+
+import org.openqa.selenium.*;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.util.concurrent.TimeUnit;
+
+public class DemoGuru99 {
+	public static void main(String[] args)
+	{
+		System.setProperty("webdriver.gecko.driver", "D:\\Work\\Selenium\\geckodriver.exe");
+		System.setProperty("webdriver.chrome.driver","D:\\Work\\Selenium\\chromedriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		
+		String  baseurl = "http://demo.guru99.com/v1/";
+		driver.get(baseurl);
+		System.out.println(driver.getTitle());
+		WebElement userName = driver.findElement(By.name("uid"));
+		WebElement passWord = driver.findElement(By.xpath("//input[@name='password']"));
+		userName.sendKeys("duc");
+		passWord.sendKeys("zxcasdqwe321");
+		System.out.println("xpath");
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.linkText("here")).click();
+		System.out.println("text()");
+		driver.findElement(By.xpath("//*[text()='Email ID']//following::input")).sendKeys("ngoc-duc");
+		System.out.println("following");
+		driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+		driver.navigate().back();
+		
+		driver.findElement(By.xpath("//*[text()='UserID']//following::input")).clear();
+		driver.findElement(By.xpath("//*[@type='text']//following::input")).sendKeys("12345678");
+		System.out.println("following");
+		driver.findElement(By.xpath("//*[text()='Password']//preceding::input")).sendKeys("abcdef");
+		System.out.println("preceding");
+		driver.findElement(By.xpath("//*[@type='text']//following::input[2]")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
+		
+//		driver.close();
+//		driver.quit();
+	}
+
+}
