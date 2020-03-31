fb8f8a9b8338faa51373edbcb04dccc3ca43285e
NGOC DUC <vungocduc@VN-HCM-D-W-0007.globalservs.com>
2020-03-31 11:13:49 +0700

    switchWindows

--- src/demo/DemoGuru99.java
+++ src/demo/DemoGuru99.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class DemoGuru99 {
@@ -19,11 +21,37 @@ public class DemoGuru99 {
 //		driver = new ChromeDriver();
 		
 		Actions builder = new Actions(driver);
-		String  baseurl = "http://demo.guru99.com/v1/";
-		driver.get(baseurl);
-		System.out.println(driver.getTitle());
+//		String  baseurl = "http://demo.guru99.com/v1/";
+//		driver.get(baseurl);
 //		driver.manage().window().maximize();
 		
+		//Launching popup site
+		driver.get("http://demo.guru99.com/popup.php");
+		driver.findElement(By.xpath("//*[contains(@href,'popup.php')]")).click();
+		Thread.sleep(2000);
+		String MainWindow = driver.getWindowHandle();
+		Set<String> s1 = driver.getWindowHandles();
+		Iterator<String> i1 = s1.iterator();
+		while(i1.hasNext())			
+        {		
+            String ChildWindow=i1.next();		
+            		
+            if(!MainWindow.equalsIgnoreCase(ChildWindow))			
+            {    		
+                 
+                // Switching to Child window
+                driver.switchTo().window(ChildWindow);	 
+                System.out.println("ChildWindow");
+                driver.findElement(By.name("emailid")).sendKeys("gaurav.3n@gmail.com");     
+                driver.findElement(By.name("btnLogin")).click();			
+                driver.close();		
+            }		
+        }		
+		// Switching to Parent window i.e Main Window.
+        driver.switchTo().window(MainWindow);
+        System.out.println("MainWindow");
+        
+        driver.findElement(By.xpath("//a[contains(text(), 'Bank')]")).click();
 		WebElement userName = driver.findElement(By.name("uid"));
 		WebElement passWord = driver.findElement(By.xpath("//input[@name = 'password']"));
 		userName.sendKeys("duc");
@@ -42,18 +70,15 @@ public class DemoGuru99 {
 		seriesOfActions.perform() ;
 		System.out.println("keyboard mouse events");
 		
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.findElement(By.linkText("here")).click();
-		System.out.println("text()");
-		driver.findElement(By.xpath("//*[text() = 'Email ID']//following::input")).sendKeys("ngoc-duc");
-		System.out.println("following");
+		System.out.println("linkText");
+		driver.findElement(By.xpath("//*[text() = 'Email ID']//following::input[1]")).sendKeys("ngoc-duc");
 		driver.navigate().back();
-		
 		driver.findElement(By.xpath("//*[text() = 'UserID']//following::input")).clear();
 		driver.findElement(By.xpath("//*[@type = 'text']//following::input")).sendKeys("12345678");
-		System.out.println("following");
 		driver.findElement(By.xpath("//*[text() ='Password']//preceding::input")).sendKeys("abcdef");
-		System.out.println("preceding");
+		System.out.println("xpath axes");
+		
 		driver.findElement(By.xpath("//*[@type='text']//following::input[2]")).click();
 		System.out.println(driver.switchTo().alert().getText());
 		driver.switchTo().alert().accept();
@@ -67,10 +92,15 @@ public class DemoGuru99 {
 //		executor.executeScript("arguments[0].click();", navBarList.get(0));
 		System.out.println("open menu");
 		driver.findElement(By.cssSelector("a[href = '../../test/link.html']")).click();
-		System.out.println("click link");
 		driver.findElement(By.cssSelector("a[href ='http://demo.guru99.com/V1/index.php']")).click();
-		System.out.println("click Bank");
+		driver.findElement(By.cssSelector("a[href = '../../test/radio.html']")).click();
 		
+		
+//		driver.findElement(By.cssSelector("a[href = '../../test/link.html']")).click();
+		
+		
+		
+        
 //		driver.quit();
 		driver.close();
 		System.out.println("close");
